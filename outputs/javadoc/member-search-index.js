47c67237d77e9db00f64bf640c6cce123db74eb2
w3ichen <w3ichen@users.noreply.github.com>
2022-02-17 17:05:29 +0000

    deploy: 3b5aa1cc006185453134147ff5746b6d1a56e7cd

---
+++ outputs/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.example.qr_go","c":"MapsActivity","l":"MapsActivity()","url":"%3Cinit%3E()"},{"p":"com.example.qr_go","c":"MapsActivity","l":"onCreate(Bundle)","url":"onCreate(android.os.Bundle)"},{"p":"com.example.qr_go","c":"MapsActivity","l":"onMapReady(GoogleMap)","url":"onMapReady(com.google.android.gms.maps.GoogleMap)"}]
---
+++ outputs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example.qr_go","l":"MapsActivity"}]
