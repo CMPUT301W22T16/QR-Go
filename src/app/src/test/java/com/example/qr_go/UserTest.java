412878f11b79546334010c6fe9da732a606382a8
mrcromero <mrcromero33@gmail.com>
2022-03-02 14:15:01 -0700

    add user qr tests

--- src/app/src/test/java/com/example/qr_go/UserTest.java
+++ src/app/src/test/java/com/example/qr_go/UserTest.java
@@ -1,5 +1,33 @@
 package com.example.qr_go;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+import java.security.NoSuchAlgorithmException;
+
 public class UserTest {
 
+    @Test
+    public void userLoginQRTest() throws NoSuchAlgorithmException {
+        User testUser = new User();
+        String loginString = testUser.getUserid()+"\n"+testUser.getPassword();
+
+        // Compare hashed login data and user's login QR data
+        QRCode newLoginQR = new QRCode(loginString);
+        LoginQRCode genLoginQR = testUser.getLoginQR();
+        assertEquals(newLoginQR.getHash(), genLoginQR.getHash());
+    }
+
+    @Test
+    public void userStatusQRTest() throws NoSuchAlgorithmException {
+        User testUser = new User();
+        String statusString = testUser.getUserid();
+
+        // Compare hashed status data and user's status QR data
+        QRCode newStatusQR = new QRCode(statusString);
+        StatusQRCode genStatusQR = testUser.getStatusQR();
+        assertEquals(newStatusQR.getHash(), genStatusQR.getHash());
+    }
 }
