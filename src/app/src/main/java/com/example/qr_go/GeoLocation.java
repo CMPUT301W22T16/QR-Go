d014da7aa3eaf7bef04d5ae432144b06b3a0c984
Landyn <landynpugh@gmail.com>
2022-03-02 14:04:33 -0700

    Added GeoLocation class

---
+++ src/app/src/main/java/com/example/qr_go/GeoLocation.java
@@ -0,0 +1,14 @@
+package com.example.qr_go;
+
+public class GeoLocation {
+    private Location location;
+    private Integer locatedQRId;
+
+    public GeoLocation() {
+
+    }
+
+    public String getAddress() {
+
+    }
+}
