d98d7333bd81be71f3e5c464c3c618ccba002049
Landyn <landynpugh@gmail.com>
2022-03-08 00:57:24 -0700

    Completed basic function of geolocation class

--- src/app/src/main/java/com/example/qr_go/GeoLocation.java
+++ src/app/src/main/java/com/example/qr_go/GeoLocation.java
@@ -1,53 +1,45 @@
 package com.example.qr_go;
-import android.app.Activity;
-import android.app.Application;
-import android.content.Context;
 import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 
 
-public class GeoLocation extends Application{
-    protected Context currentContext;
+public class GeoLocation{
+    /**
+     * Class for GeoLocation, contains location data such as address, latitude, longitude, and the
+     * QR ID associated with the specified location.
+     * @param locatedQRID
+     * @author LPugh
+     */
+
+    /*
+    we need to compress the images to the database
+    we need to be able to load the image into the activity
+     */
     private double longitude;
     private double latitude;
     private Address address;
     private Integer locatedQRId;
 
-    public GeoLocation(double latitude, double longitude) {
-        this.currentContext = getApplicationContext();
-
-        Geocoder geocoder = new Geocoder(currentContext, Locale.getDefault());
-        List<Address> addressList = null;
-        try {
-            addressList = geocoder.getFromLocation(latitude, longitude, 1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        this.address = addressList.get(0);
-        this.longitude=longitude;
-        this.latitude=latitude;
+    public GeoLocation(Integer locatedQRId) {
+        this.locatedQRId = locatedQRId;
     }
-//    public GeoLocation(Context context, Address address) {
-//        Geocoder geocoder = new Geocoder(currentContext, Locale.getDefault());
-//        this.currentContext=context;
-//        this.address=address;
-//    }
-
 
     public Address getAddress() {
         return this.address;
     }
-
     public double getLongitude() {
         return this.longitude;
     }
     public double getLatitude() {
         return this.latitude;
+    }
+    public Integer getQRId() {
+        return this.locatedQRId;
+    }
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    public void setCoords(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
     }
 }
