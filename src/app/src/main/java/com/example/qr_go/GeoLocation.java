17b4844218499d1225991f7ef77603724298e141
Landyn Pugh <lpugh@ualberta.ca>
2022-03-07 21:53:24 -0700

    Added methods to geolocation class. Tests WIP

--- src/app/src/main/java/com/example/qr_go/GeoLocation.java
+++ src/app/src/main/java/com/example/qr_go/GeoLocation.java
@@ -1,14 +1,53 @@
 package com.example.qr_go;
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 
-public class GeoLocation {
-    private Location location;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+
+public class GeoLocation extends Application{
+    protected Context currentContext;
+    private double longitude;
+    private double latitude;
+    private Address address;
     private Integer locatedQRId;
 
-    public GeoLocation() {
+    public GeoLocation(double latitude, double longitude) {
+        this.currentContext = getApplicationContext();
 
+        Geocoder geocoder = new Geocoder(currentContext, Locale.getDefault());
+        List<Address> addressList = null;
+        try {
+            addressList = geocoder.getFromLocation(latitude, longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.address = addressList.get(0);
+        this.longitude=longitude;
+        this.latitude=latitude;
+    }
+//    public GeoLocation(Context context, Address address) {
+//        Geocoder geocoder = new Geocoder(currentContext, Locale.getDefault());
+//        this.currentContext=context;
+//        this.address=address;
+//    }
+
+
+    public Address getAddress() {
+        return this.address;
     }
 
-    public String getAddress() {
-
+    public double getLongitude() {
+        return this.longitude;
+    }
+    public double getLatitude() {
+        return this.latitude;
     }
 }
