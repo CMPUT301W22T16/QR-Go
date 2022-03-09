8dc40ba05d34f14ba14cd1df7bd313e804a04dcc
Landyn Pugh <lpugh@ualberta.ca>
2022-03-09 16:05:51 -0700

    Added PlayerInfoActivity

---
+++ src/app/src/main/java/com/example/qr_go/PlayerInfoActivity.java
@@ -0,0 +1,27 @@
+package com.example.qr_go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlayerInfoActivity extends AppCompatActivity {
+    private User selectedUser;
+    private GameQRCode myQRCodes;
+
+    public void onCreate() {
+
+    }
+
+    public Integer getTotalScore(){
+
+    }
+    public Integer getQRCount() {
+
+    }
+    public Integer getHighestQRScore() {
+
+    }
+    public void showScannedQRList() {
+
+    }
+
+
+}
