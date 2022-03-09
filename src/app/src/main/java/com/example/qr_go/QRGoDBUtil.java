36f321e7ecd803757cf6b25cc4716eef82834cd6
Darius Fang <56571687+DarFang@users.noreply.github.com>
2022-03-09 00:08:31 -0700

    added GameQRCode add to db functionality

--- src/app/src/main/java/com/example/qr_go/QRGoDBUtil.java
+++ src/app/src/main/java/com/example/qr_go/QRGoDBUtil.java
@@ -11,41 +11,111 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.WriteBatch;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
+@RequiresApi(api = Build.VERSION_CODES.O)
+/**
+ * QRGODBUtil is a util class that helps with manipulating the db and getting values of the db.
+ */
 public class QRGoDBUtil {
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void AddQRtoDatabase(String qrID, String UserID)throws NoSuchAlgorithmException {
-        FirebaseFirestore db = MapsActivity.db;
+    /**global variables
+     *
+     */
+    ArrayList<GameQRCode>  QRCodeList = new ArrayList<GameQRCode>();
+    GameQRCode temp, qrcode;
+    String qrID, UserID;
+    Player player;
+    FirebaseFirestore db = MapsActivity.db;
+    WriteBatch batch = db.batch();
 
-        QRCode temp = new QRCode(qrID);
+    {
+        try {
+            player = new Player();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    /** Gets QR  from database then it executes add QR to database
+     *
+     * @Author Darius Fang
+     */
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    void GrabQRFromDatabase(String qrIDInput, String UserIDInput)throws NoSuchAlgorithmException {
+        qrcode = new GameQRCode();
+        UserID = UserIDInput;
+        qrID = qrIDInput;
+        temp = new GameQRCode(qrID);
         String hash = temp.getHash();
         DocumentReference docRef = db.collection("GameQRCodes").document(hash);
+        QRCodeList.clear();
         docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-
             @Override
+            @RequiresApi(api = Build.VERSION_CODES.O)
             public void onSuccess(DocumentSnapshot documentSnapshot) {
-                GameQRCode code = documentSnapshot.toObject(GameQRCode.class);
-                /** NEED TO CHECK IF USERID IS REGISTERED
-                 *
-                 *
-                 *
-                 *
-                 *
-                 * **/
-                db.collection("GameQRCodes").document(code.getHash()).set(code).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d("SUCCESS", "Data has been added successfully!");
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d("FAILURE", "Data could not be added: " + e.toString());
-                    }
-                });
+                try {
+                    GameQRCode qrcode = documentSnapshot.toObject(GameQRCode.class);
+                    QRCodeList.add(qrcode);
+                    AddQRtoDatabase();
+                }catch (Exception e){
+                    /** sometimes the db picks up that it exists while in fact it does not... strange
+                     */
+                    QRCodeList.clear();
+                    AddQRtoDatabase();
+                }
             }
-        });
+
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                AddQRtoDatabase();
+            }
+        });;
+
+
+    }
+
+    /** ADDs QR to database
+     * IF the QR exists, it will update the QR, otherwise it will add it
+     * it also adds the userid
+     *
+     * @Author Darius Fang
+     */
+    void AddQRtoDatabase(){
+        if (QRCodeList.isEmpty()){
+            qrcode = temp;
+        }
+        else{
+            qrcode = QRCodeList.get(0);
+        }
+        if (!qrcode.getUserIds().contains(UserID)) {
+            qrcode.addUser(UserID);
+            db.collection("GameQRCodes").document(qrcode.getHash()).set(qrcode);
+
+        }
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    void UpdateUser() throws NoSuchAlgorithmException{
+
+        player.addQRCode(qrcode);
+        batch.set(db.collection("Players").document(player.getUsername()), player);
+
+
+    }
+
+    /** this does not work help
+     *
+     * @throws NoSuchAlgorithmException
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    void test1()throws NoSuchAlgorithmException{
+        GrabQRFromDatabase("BFG5DGW54\n", "Darius2");
     }
 }
