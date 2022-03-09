24251878692984c09333e13f8fd326b5c6f5c6ef
Landyn Pugh <lpugh@ualberta.ca>
2022-03-09 10:34:18 -0700

    Removed unneeded comments from GeoLocation

--- src/app/src/main/java/com/example/qr_go/GeoLocation.java
+++ src/app/src/main/java/com/example/qr_go/GeoLocation.java
@@ -10,10 +10,6 @@ public class GeoLocation{
      * @author LPugh
      */
 
-    /*
-    we need to compress the images to the database
-    we need to be able to load the image into the activity
-     */
     private double longitude;
     private double latitude;
     private Address address;
