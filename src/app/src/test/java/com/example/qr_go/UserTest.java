10be839deb1917bce53efce15a27b41710b61bb3
mrcromero <mrcromero33@gmail.com>
2022-03-10 18:33:02 -0700

    remove unnecessary tests

--- src/app/src/test/java/com/example/qr_go/UserTest.java
+++ src/app/src/test/java/com/example/qr_go/UserTest.java
@@ -16,26 +16,4 @@ public class UserTest {
         testUser = new Player();
     }
 
-    @Ignore
-    @Test
-    public void userLoginQRTest()  {
-        String loginString = testUser.getUserid()+"\n"+testUser.getPassword();
-
-        // Compare hashed login data and user's login QR data
-        QRCode newLoginQR = new QRCode(loginString);
-        LoginQRCode genLoginQR = testUser.getLoginQR();
-        assertEquals(newLoginQR.getHash(), genLoginQR.getHash());
-    }
-
-    @Ignore
-    @Test
-    public void userStatusQRTest()  {
-        String statusString = testUser.getUserid();
-
-        // Compare hashed status data and user's status QR data
-        QRCode newStatusQR = new QRCode(statusString);
-        StatusQRCode genStatusQR = testUser.getStatusQR();
-        assertEquals(newStatusQR.getHash(), genStatusQR.getHash());
-    }
-
 }
