a46cd3479c1053d88fef46249fcdda5c253c17f0
Landyn <landynpugh@gmail.com>
2022-03-11 22:49:42 -0700

    Configured user info in layout

--- src/app/src/main/java/com/example/qr_go/PlayerInfoActivity.java
+++ src/app/src/main/java/com/example/qr_go/PlayerInfoActivity.java
@@ -1,12 +1,15 @@
 package com.example.qr_go;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -25,17 +28,21 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+
 public class PlayerInfoActivity extends AppCompatActivity {
     private User selectedUser;
     private String selectedUserID;
-    private GameQRCode myQRCodes;
+    private ArrayList<String> myQRCodeIDs;
+    private ArrayList<GameQRCode> myQRCodes;
     private Integer userScore;
-
     final String TAG = "Sample";
     Button addCityButton;
+
     EditText addCityEditText;
     EditText addProvinceEditText;
     FirebaseFirestore userDBInst;
+
 
 
     @Override
@@ -43,7 +50,12 @@ public class PlayerInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_info);
 
-        //reused from UsernameGenerator.java
+        TextView usernameText = findViewById(R.id.userNameText);
+        TextView qrNumText = findViewById(R.id.numOfQRCodes);
+        TextView totalScoreText = findViewById(R.id.userTotalScore);
+        TextView highestScoreText = findViewById(R.id.userHighScore);
+
+
         final Boolean[] valid = new Boolean[1];
         userDBInst = FirebaseFirestore.getInstance();
 
@@ -57,10 +69,43 @@ public class PlayerInfoActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 selectedUser.setUsername((String) document.getData().get("username"));
+                                userScore = (Integer) document.getData().get("totalScore");
+                                myQRCodeIDs = (ArrayList<String>) document.getData().get("scannedQRCodeIds");
                             }
                         }
                     }
                 });
+        usernameText.setText(selectedUser.getUsername());
+        qrNumText.setText(Integer.toString(myQRCodeIDs.size()));
+        totalScoreText.setText(userScore);
+
+
+        for(int i = 0; i < myQRCodeIDs.size(); i++) {
+            //Create loop to grab and instantiate all QR codes scanned by a user
+            userDBInst.collection("GameQRCodes")
+                    .whereEqualTo(myQRCodeIDs.get(i), selectedUserID)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @RequiresApi(api = Build.VERSION_CODES.O)
+                        @Override
+                        // code from https://stackoverflow.com/questions/65465335/get-specific-field-from-firestore-with-whereequalto
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    GameQRCode tempQR = new GameQRCode((String)document.getData().get("hash"));
+                                    myQRCodes.add(tempQR);
+                                    //get highest scoring QR
+                                }
+                            }
+                        }
+                    });
+        }
+
+
+
+
+
+
 
         }
 
@@ -68,16 +113,17 @@ public class PlayerInfoActivity extends AppCompatActivity {
 
 
     public Integer getTotalScore(){
-        return selectedUser.getTotalScore();
+        return userScore;
     }
     public Integer getQRCount() {
-        return selectedUser.getScannedQRCodeIds().size();
+        return myQRCodeIDs.size();
     }
     public Integer getHighestQRScore() {
+
         return 0;
     }
     public void showScannedQRList() {
-        selectedUser.getScannedQRCodeIds();
+        return;
     }
 
 
---
+++ src/app/src/main/java/com/example/qr_go/QRGoDBUtil.java
@@ -0,0 +1,119 @@
+package com.example.qr_go;
+
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.WriteBatch;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
+
+@RequiresApi(api = Build.VERSION_CODES.O)
+/**
+ * QRGODBUtil is a util class that helps with manipulating the db and getting values of the db.
+ */
+public class QRGoDBUtil {
+    /**global variables
+     *
+     */
+    ArrayList<GameQRCode>  QRCodeList = new ArrayList<GameQRCode>();
+    FirebaseFirestore db = MapsActivity.db;
+    /** Gets QR  from database then it executes add QR to database
+     *
+     * @Author Darius Fang
+     */
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    void updateScannedQRtoDB(GameQRCode gameqrcode, Player  player, QRPhoto qrphoto, GeoLocation geolocation) {
+
+        DocumentReference docRef = db.collection("GameQRCodes").document(gameqrcode.getHash());
+        QRCodeList.clear();
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                try {
+                    GameQRCode qrcode = documentSnapshot.toObject(GameQRCode.class);
+                    QRCodeList.add(qrcode);
+                    updateScannedQRtoDBContinue(null, player, qrphoto, geolocation);
+                }catch (Exception e){
+                    /** sometimes the db picks up that it exists while in fact it does not... strange
+                     */
+                    QRCodeList.clear();
+                    updateScannedQRtoDBContinue(gameqrcode, player, qrphoto, geolocation);
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                updateScannedQRtoDBContinue(gameqrcode, player, qrphoto, geolocation);
+            }
+        });;
+
+
+    }
+    /** ADDs QR to database
+     * IF the QR exists, it will update the QR, otherwise it will add it
+     * it also adds the userid
+     *
+     * @Author Darius Fang
+     */
+    /**
+     *
+
+     * @param gameqrcode
+     * @param player
+     * @param qrphoto
+     * @param geolocation
+     */
+    void updateScannedQRtoDBContinue(GameQRCode gameqrcode, Player player, QRPhoto qrphoto, GeoLocation geolocation){
+        String UserID = player.getUserid();
+        if (QRCodeList.isEmpty()){
+            gameqrcode.addUser(UserID);
+            db.collection("GameQRCodes").document(gameqrcode.getHash()).set(gameqrcode);
+        }
+        else{
+            GameQRCode newGameQRCode = QRCodeList.get(0);
+            // Add GameQR to DB
+            if (!newGameQRCode.getUserIds().contains(UserID)) {
+                newGameQRCode.addUser(UserID);
+                db.collection("GameQRCodes").document(newGameQRCode.getHash()).update("userIds", newGameQRCode.getUserIds());
+
+            }
+            // Update Player to DB
+
+        }
+        if (!player.getScannedQRCodeIds().contains(gameqrcode.getId())){
+            player.addQRCode(gameqrcode);
+            db.collection("Players").document(player.getUserid()).set(player);
+        }
+        // Update Photo to DB
+        if (qrphoto != null){
+            db.collection("QRPhotos").document(qrphoto.getQRID()).set(qrphoto);
+        }
+        //  Update GeoLocation to DB
+        if (geolocation != null){
+            db.collection("Geolocations").document(qrphoto.getQRID()).set(qrphoto);
+        }
+
+    }
+
+
+
+    void test1(){
+        GameQRCode qrcode = new GameQRCode("BFG5DGW54\n");
+        Player player = new Player();
+        db.collection("Players").document(player.getUserid()).set(player);
+        updateScannedQRtoDB(qrcode,player, null, null);
+
+    }
+}
