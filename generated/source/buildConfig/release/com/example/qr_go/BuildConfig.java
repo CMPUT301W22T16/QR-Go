2b06c5fee497a3ae742c76182d042d090f46e8da
LandynPugh <LandynPugh@users.noreply.github.com>
2022-03-13 09:53:37 +0000

    deploy: fea6069f6a737dd333b349373aeda09931d3757e

---
+++ generated/source/buildConfig/release/com/example/qr_go/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.qr_go;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.qr_go";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  public static final String MAPS_API_KEY = "AIzaSyBwdjiGdUudmbI1R4sFmg3Dnzpu8XvrJ9w";
+}
