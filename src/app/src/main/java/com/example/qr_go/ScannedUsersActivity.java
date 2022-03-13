a661869dcfe05effcb52b75fc601fb243498aeb2
matzit20 <54906052+mzita@users.noreply.github.com>
2022-03-13 14:21:46 -0600

    built ScannedUsersActivity

---
+++ src/app/src/main/java/com/example/qr_go/ScannedUsersActivity.java
@@ -0,0 +1,18 @@
+package com.example.qr_go;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScannedUsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanned_users);
+
+
+
+    }
+
+}
