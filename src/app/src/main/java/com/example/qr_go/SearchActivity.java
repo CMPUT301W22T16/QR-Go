ce102f45963c094e00d26be0bfc778b91281ed99
mrcromero <mrcromero33@gmail.com>
2022-03-21 13:37:57 -0600

    add default no-location view

--- src/app/src/main/java/com/example/qr_go/SearchActivity.java
+++ src/app/src/main/java/com/example/qr_go/SearchActivity.java
@@ -22,7 +22,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -45,8 +44,10 @@ public class SearchActivity extends BaseActivity {
     private Location userLocation;
     LocationManager locationManager;
     // String arrays holding the sorting options
-    private String[] qrSortOptions = {"Score", "Proximity"};
+    private String[] qrSortOptions = {"Score"};
     private final String[] playerSortOptions = {"Total Score", "# QR Codes", "Unique Score"};
+    // Adapter for qr sort options as it may be updated with location
+    ArrayAdapter<String> qrSortOptionAdapter;
     // Current fragment being displayed
     private Integer currentFragment = 0;
     // Current position that sort spinner is selected as
@@ -59,27 +60,22 @@ public class SearchActivity extends BaseActivity {
     // The search bar
     private static EditText searchBar;
 
+    // The context
+    Context context;
+
 
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         initializeNavbar();
-        // Request location permissions
-        // Code taken from NewGameQRActivity.java file
-        try {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
-                    ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
-            } else {
-                // If has permissions, set location by default
-                setUserLocation();
-//                disableLocation();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+        // Disable the location first so that there is at least a view that the user can see
+        // If location is enabled, then the views are updated and the user is able to sort
+        // using proximity when the location manager responds.
+        disableLocation();
 
         Button backButton = (Button) findViewById(R.id.back_button);
         searchBar = (EditText) findViewById(R.id.search_bar);
@@ -95,106 +91,19 @@ public class SearchActivity extends BaseActivity {
     }
 
     /**
-     * @return User data to display on fragments
-     */
-    public static ArrayList<UserListDisplayContainer> getUserDisplays() {
-        return userDisplays;
-    }
-
-    /**
-     * @return QR data to display on fragments
-     */
-    public static ArrayList<QRListDisplayContainer> getQrDisplays() {
-        return qrDisplays;
-    }
-
-    /**
-     * Returns the search bar for use in the fragments
-     * @return The search bar EditText
-     */
-    public static EditText getSearchBar() {
-        return searchBar;
-    }
-
-    /**
-     * Utility function for clearing search bar and minimizing keyboard
-     * https://stackoverflow.com/a/2342856
-     */
-    private void clearSearchBar() {
-        searchBar.getText().clear();
-        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        mgr.hideSoftInputFromWindow(searchBar.getWindowToken(), 0);
-    }
-
-    /**
-     * Once user has accepted/denied permission access
-     * If permission granted, set the location of the QR code
-     * If permission denied, disable the location
-     *
-     * Code from NewGameQRActivity.java
-     *
-     * @param requestCode
-     * @param permissions
-     * @param grantResults
-     */
-    @RequiresApi(api = Build.VERSION_CODES.P)
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == LOCATION_REQUEST_CODE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // If permission granted, set the location
-                setUserLocation();
-            } else {
-                disableLocation();
-            }
-        }
-    }
-
-    /**
      * If location is disabled, do not allow user to sort by proximity
      */
+    @RequiresApi(api = Build.VERSION_CODES.P)
     private void disableLocation() {
-        qrSortOptions = new String[]{"Score"};
         userLocation = null;
         startViews();
     }
 
     /**
-     * Assuming that location permissions is granted
-     * The current location of the user is received
-     * The location is then set in the game qr code
-     *
-     * Code from NewGameQRActivity.java
+     * Initialize the views
      */
     @SuppressLint("MissingPermission")
     @RequiresApi(api = Build.VERSION_CODES.P)
-    private void setUserLocation() {
-        try {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-                    ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                // Acquire a reference to the system Location Manager
-                locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-                // Define a listener that responds to location updates
-                LocationListener locationListener = new LocationListener() {
-                    public void onLocationChanged(Location location) {
-                        userLocation = new Location(location);
-                        startViews();
-                        locationManager.removeUpdates(this);
-                    }
-                };
-                // Register the listener with the Location Manager to receive location updates
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-            }
-
-        } catch (NoSuchMethodError e) {
-            e.printStackTrace();
-            Toast.makeText(this, "failed to get location", Toast.LENGTH_LONG).show();
-            // disable location
-            disableLocation();
-        }
-    }
-
-    @SuppressLint("MissingPermission")
     private void startViews() {
         // Initialize database references
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -210,7 +119,7 @@ public class SearchActivity extends BaseActivity {
         qrSortOptionsDataList.addAll(Arrays.asList(qrSortOptions));
         ArrayList<String> playerSortOptionsDataList = new ArrayList<>();
         playerSortOptionsDataList.addAll(Arrays.asList(playerSortOptions));
-        ArrayAdapter<String> qrSortOptionAdapter = new ArrayAdapter<>(this,
+        qrSortOptionAdapter = new ArrayAdapter<>(this,
                 R.layout.spinner_item, qrSortOptionsDataList);
         ArrayAdapter<String> playerSortOptionAdapter = new ArrayAdapter<>(this,
                 R.layout.spinner_item, playerSortOptionsDataList);
@@ -322,37 +231,153 @@ public class SearchActivity extends BaseActivity {
         });
         qrColRef.whereNotEqualTo("deleted", true).get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @RequiresApi(api = Build.VERSION_CODES.P)
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 qrDisplays.clear();
                 for(DocumentSnapshot snapshot : queryDocumentSnapshots) {
-                    Float distance;
                     Map qrLocationMap = (Map) snapshot.get("geoLocation");
-                    // If no recorded location, set distance as null, else calculate the distance
-                    if (qrLocationMap == null || userLocation == null) {
-                        distance = null;
-                    } else {
-                        Double qrLat = (Double) qrLocationMap.get("latitude");
-                        Double qrLon = (Double) qrLocationMap.get("longitude");
-                        Location qrLocation = new Location("qr");
-                        qrLocation.setLatitude(qrLat);
-                        qrLocation.setLongitude(qrLon);
-                        distance = qrLocation.distanceTo(userLocation);
+                    // Set distances as null for now. If the user's location is found, then this
+                    // will be updated later
+                    Double qrLat = null;
+                    Double qrLon = null;
+                    if (qrLocationMap != null) {
+                        qrLat = (Double) qrLocationMap.get("latitude");
+                        qrLon = (Double) qrLocationMap.get("longitude");
                     }
                     QRListDisplayContainer qrToDisplay =
                             new QRListDisplayContainer(
                                     snapshot.get("score", Integer.class),
                                     snapshot.get("id", String.class),
-                                    distance
+                                    qrLat,
+                                    qrLon,
+                                    null
                             );
                     qrDisplays.add(qrToDisplay);
                 }
                 // Update the fragments after getting documents is done
                 searchPagerAdapter.updateSort(currentFragment, sortPosition);
-
+                // Request location permissions
+                // Code taken from NewGameQRActivity.java file
+                try {
+                    if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                            ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+                    } else {
+                        // If has permissions, set location by default
+                        setUserLocation();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
 
+    }
+
+    /**
+     * Once user has accepted/denied permission access
+     * If permission granted, set the location of the QR code
+     * If permission denied, disable the location
+     *
+     * Code from NewGameQRActivity.java
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // If permission granted, set the location
+                setUserLocation();
+            }
+        }
+    }
+
+
+    /**
+     * Assuming that location permissions is granted
+     * The current location of the user is received
+     * The location is then set and a new sort option
+     * is provided for the user
+     *
+     * Code from NewGameQRActivity.java
+     */
+    @SuppressLint("MissingPermission")
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void setUserLocation() {
+        try {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                // Acquire a reference to the system Location Manager
+                locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+                // Define a listener that responds to location updates
+                LocationListener locationListener = new LocationListener() {
+                    public void onLocationChanged(Location location) {
+                        userLocation = new Location(location);
+                        addLocation();
+                        locationManager.removeUpdates(this);
+                    }
+                };
+                // Register the listener with the Location Manager to receive location updates
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            }
+
+        } catch (NoSuchMethodError e) {
+            e.printStackTrace();
+            Toast.makeText(this, "failed to get location", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * Add the option to sort by proximity and update the qrcode array
+     */
+    private void addLocation() {
+        for(QRListDisplayContainer qr : qrDisplays) {
+            if (qr.getLat() != null && qr.getLon() != null) {
+                Location qrLocation = new Location("qr");
+                qrLocation.setLatitude(qr.getLat());
+                qrLocation.setLongitude(qr.getLon());
+                qr.setDistance(qrLocation.distanceTo(userLocation));
+            }
+        }
+        // Add proximity as a search option;
+        qrSortOptionAdapter.add("Proximity");
+    }
+
+    /**
+     * @return User data to display on fragments
+     */
+    public static ArrayList<UserListDisplayContainer> getUserDisplays() {
+        return userDisplays;
+    }
+
+    /**
+     * @return QR data to display on fragments
+     */
+    public static ArrayList<QRListDisplayContainer> getQrDisplays() {
+        return qrDisplays;
+    }
+
+    /**
+     * Returns the search bar for use in the fragments
+     * @return The search bar EditText
+     */
+    public static EditText getSearchBar() {
+        return searchBar;
+    }
+
+    /**
+     * Utility function for clearing search bar and minimizing keyboard
+     * https://stackoverflow.com/a/2342856
+     */
+    private void clearSearchBar() {
+        searchBar.getText().clear();
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(searchBar.getWindowToken(), 0);
     }
 
 }
