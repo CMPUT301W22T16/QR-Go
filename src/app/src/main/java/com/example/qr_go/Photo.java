e0bb2b84225d76aaf0d3707122b5656138970479
Darius Fang <56571687+DarFang@users.noreply.github.com>
2022-03-21 22:30:03 -0600

    undo photo edit

--- src/app/src/main/java/com/example/qr_go/Photo.java
+++ src/app/src/main/java/com/example/qr_go/Photo.java
@@ -12,9 +12,9 @@ public class Photo {
     we need to compress the images to the database
     we need to be able to load the image into the activity
      */
-    protected String photoRef;
-    Photo(String photoRef){
-        this.photoRef = photoRef;
-        
+    protected byte[] image;
+    Photo(byte[] image){
+        this.image = image;
+
     }
 }
