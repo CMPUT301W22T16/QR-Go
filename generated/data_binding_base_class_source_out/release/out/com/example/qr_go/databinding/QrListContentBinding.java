8b7fd0c4fdc2994acb77194239a0f3aa10a6a1e6
DarFang <DarFang@users.noreply.github.com>
2022-04-02 22:23:08 +0000

    deploy: 64a806cfb625c6d6af4984a7b900aaf705689ba2

--- generated/data_binding_base_class_source_out/release/out/com/example/qr_go/databinding/QrListContentBinding.java
+++ generated/data_binding_base_class_source_out/release/out/com/example/qr_go/databinding/QrListContentBinding.java
@@ -25,9 +25,6 @@ public final class QrListContentBinding implements ViewBinding {
   public final MaterialButton qrDelButton;
 
   @NonNull
-  public final TextView qrHoodView;
-
-  @NonNull
   public final TextView qrIdView;
 
   @NonNull
@@ -37,11 +34,10 @@ public final class QrListContentBinding implements ViewBinding {
   public final TextView qrScoreView;
 
   private QrListContentBinding(@NonNull RelativeLayout rootView,
-      @NonNull MaterialButton qrDelButton, @NonNull TextView qrHoodView, @NonNull TextView qrIdView,
+      @NonNull MaterialButton qrDelButton, @NonNull TextView qrIdView,
       @NonNull ShapeableImageView qrPicture, @NonNull TextView qrScoreView) {
     this.rootView = rootView;
     this.qrDelButton = qrDelButton;
-    this.qrHoodView = qrHoodView;
     this.qrIdView = qrIdView;
     this.qrPicture = qrPicture;
     this.qrScoreView = qrScoreView;
@@ -80,12 +76,6 @@ public final class QrListContentBinding implements ViewBinding {
         break missingId;
       }
 
-      id = R.id.qr_hood_view;
-      TextView qrHoodView = ViewBindings.findChildViewById(rootView, id);
-      if (qrHoodView == null) {
-        break missingId;
-      }
-
       id = R.id.qr_id_view;
       TextView qrIdView = ViewBindings.findChildViewById(rootView, id);
       if (qrIdView == null) {
@@ -104,8 +94,8 @@ public final class QrListContentBinding implements ViewBinding {
         break missingId;
       }
 
-      return new QrListContentBinding((RelativeLayout) rootView, qrDelButton, qrHoodView, qrIdView,
-          qrPicture, qrScoreView);
+      return new QrListContentBinding((RelativeLayout) rootView, qrDelButton, qrIdView, qrPicture,
+          qrScoreView);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
